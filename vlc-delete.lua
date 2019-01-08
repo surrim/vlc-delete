c313504038fe1ebd3837f69a98834760a3994638
surrim <surrim@happyhydro.org>
2019-01-08 21:59:56 +0100

    updated copyright year

--- vlc-delete.lua
+++ vlc-delete.lua
@@ -1,5 +1,5 @@
 --[[
-	Copyright 2015-2016 surrim
+	Copyright 2015-2019 surrim
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
